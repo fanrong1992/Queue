e01b65420cfd1a668041e47997c203890cd287e8
fanrong1992 <fanrong1992@gmail.com>
2022-04-29 11:12:47 +0800

    add detection point info

--- app/src/main/java/com/example/queue/ReserveDetectionActivity.java
+++ app/src/main/java/com/example/queue/ReserveDetectionActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ReserveDetectionActivity extends AppCompatActivity {
@@ -15,6 +18,19 @@ public class ReserveDetectionActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String point = intent.getStringExtra("Point");
-        Toast.makeText(getApplicationContext(), "You clicked " + point, Toast.LENGTH_SHORT).show();
+
+        TextView pointName = (TextView) findViewById(R.id.pointName);
+        pointName.setText(point);
+
+        TextView queueInfo = (TextView) findViewById(R.id.queueInfo);
+        queueInfo.setText("当前排队人数 20 人，预计排队时间 40 分钟");
+
+        //Button queue = (Button) findViewById(R.id.queue);
+    }
+
+    public void Queue(View v) {
+        TextView queueInfo = (TextView) findViewById(R.id.queueInfo);
+        queueInfo.setText("21 号，您前面还有 20 人，预计排队时间 40 分钟");
+        Toast.makeText(getApplicationContext(), "排队成功！", Toast.LENGTH_SHORT).show();
     }
 }
